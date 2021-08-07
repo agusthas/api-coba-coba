dc68b897cff3ba8e524a9e1ac373de976866e060
agusthas <suzumantan@gmail.com>
2021-08-07 22:18:11 +0700

    add validate method

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,6 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/modules/users/users.service';
 
 import { AuthService } from './auth.service';
@@ -6,8 +8,9 @@ import { AuthService } from './auth.service';
 describe('AuthService', () => {
   let service: AuthService;
 
-  const mockedUsersService = {};
-
+  const mockedUsersService = {
+    getByUsername: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -24,5 +27,49 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validate', () => {
+    const user = {
+      id: 'a uuid',
+      name: 'John Doe',
+      username: 'johndoe',
+      password: 'hashed',
+    };
+    let spy: jest.SpyInstance;
+
+    beforeEach(() => {
+      mockedUsersService.getByUsername.mockResolvedValue(user);
+      spy = jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(async () => Promise.resolve(true));
+    });
+    it('should validate user', async () => {
+      await service.validate('johndoe', 'password');
+
+      expect(mockedUsersService.getByUsername).toBeCalledWith('johndoe');
+      expect(spy).toBeCalledTimes(1);
+    });
+
+    it('should return serialized user (no password)', async () => {
+      const result = await service.validate('johndoe', 'password');
+
+      expect(result).not.toHaveProperty('password');
+    });
+
+    it('should throw an error if given invalid credentials', async () => {
+      mockedUsersService.getByUsername
+        .mockRejectedValueOnce(new Error())
+        .mockResolvedValueOnce(user);
+
+      spy.mockImplementationOnce(() => null).mockRejectedValueOnce(false);
+
+      await expect(service.validate('johndoe', 'password')).rejects.toThrow(
+        Error,
+      );
+      await expect(service.validate('johndoe', 'password')).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { plainToClass } from 'class-transformer';
 import { UsersService } from 'src/modules/users/users.service';
+
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  public async validate(username: string, password: string): Promise<UserDto> {
+    const user = await this.usersService.getByUsername(username);
+
+    const isMatch = await compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException(`Invalid credentials`);
+    }
+
+    return plainToClass(UserDto, user, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
