28f6ebb142da55a4b6bab111951f473f467888bf
agusthas <suzumantan@gmail.com>
2021-08-07 20:18:02 +0700

    add create method

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,13 +1,34 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { QueryFailedError } from 'typeorm';
 
+import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUser = {
+    name: 'John Doe',
+    username: 'johndoe',
+    password: 'password',
+  };
+
+  const mockUserRepo = {
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: mockUserRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,5 +36,39 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const mockId = 'a uuid';
+      mockUserRepo.create.mockReturnValue(mockUser);
+      mockUserRepo.save.mockImplementation(async (input) =>
+        Promise.resolve({
+          id: mockId,
+          ...input,
+        }),
+      );
+
+      const result = await service.create(mockUser);
+
+      expect(mockUserRepo.create).toBeCalledWith(mockUser);
+      expect(mockUserRepo.save).toBeCalledWith(mockUser);
+
+      expect(result).toEqual({
+        id: mockId,
+        ...mockUser,
+      });
+    });
+
+    it('should throw a bad request exception if duplicate username', async () => {
+      mockUserRepo.create.mockReturnValue(mockUser);
+      mockUserRepo.save.mockRejectedValue(
+        new QueryFailedError('query', [], {}),
+      );
+
+      await expect(service.create(mockUser)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
   });
 });
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  public async create(input: CreateUserDto): Promise<UserEntity> {
+    const user = this.userRepository.create(input);
+
+    try {
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new BadRequestException(error.message);
+      }
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
